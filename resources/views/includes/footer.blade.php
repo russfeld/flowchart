6f3b14146bd03e68bdf00b0e15c8bad9f011f2a4
Russell Feldhausen <russfeldh@gmail.com>
2015-09-13 18:19:04 -0500

    Changing email in footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
   <div class="container">
-    <p class="text-muted">K-State Engineering Advising System - Contact webmaster@localhost for help</p>
+    <p class="text-muted">K-State Engineering Advising System - Contact webmaster@cis.ksu.edu for help</p>
   </div>
 </footer>
