d7e624ca441fe37f583e74be9d87c7d104a572be
Russell Feldhausen <russfeldh@gmail.com>
2016-03-05 18:56:25 -0600

    Minified and ready for testing

--- resources/views/advising/_message.blade.php
+++ resources/views/advising/_message.blade.php
@@ -1,2 +1,2 @@
 <br>
-<div class="alert alert-info" role="alert">The CIS Department is holding <b>Group Advising</b> sessions this year. Click <a href="{{ url('/help/groupsession') }}" class="alert-link"><b>here</b></a> to learn more!</div>
+<div class="alert alert-danger" role="alert">The CIS Department is holding <b>Group Advising</b> sessions this year. Click <a href="{{ url('/help/groupsession') }}" class="alert-link"><b>here</b></a> to learn more! Please attend a group session if possible unless you require an in-depth advising appointment. This will help us keep individual times available for students who need them most.</div>
