6ce8f0bd6db6eacf0830db82456faebe83a9f113
Russell Feldhausen <russfeldh@gmail.com>
2016-07-15 12:56:51 -0500

    Some actual output from console command

--- app/Console/Commands/PostDeploy.php
+++ app/Console/Commands/PostDeploy.php
@@ -38,11 +38,17 @@ class PostDeploy extends Command
      */
     public function handle()
     {
+        $this->info('PostDeploy routine beginning...');
         $settings = array("groupsessionenabled", "showmessage", "navbar_showcourses", "navbar_showflowcharts", "navbar_showgroupsession");
+        $bar = $this->output->createProgressBar(count($settings));
         foreach($settings as $setting){
           if(!DbConfig::has($setting)){
             DbConfig::store($setting, false);
           }
+          $bar->advance();
         }
+        $bar->finish();
+        $this->line('');
+        $this->info('PostDeploy routine complete!');
     }
 }
