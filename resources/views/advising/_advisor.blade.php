93c70b7154fc96308155935180833e7c2045dd99
Russell Feldhausen <russfeldh@gmail.com>
2018-01-14 18:44:35 -0600

    Rework advisor panel'

--- resources/views/advising/_advisor.blade.php
+++ resources/views/advising/_advisor.blade.php
@@ -1,17 +1,25 @@
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bg-light-purple rounded">
-<div class="col-md-2 col-sm-2 col-lg-2 hidden-xs center-block" style="height: auto">
-	<img class="img-responsive advisor-img hidden-xs pull-right" src="{{ url($advisor->pic) }}" alt="{{ $advisor->name . 'picture' }}">
-</div>
-<div class="col-md-10 col-sm-10 col-lg-10 col-xs-12">
+<img class="img-responsive advisor-img hidden-xs pull-left" src="{{ url($advisor->pic) }}" alt="{{ $advisor->name . 'picture' }}">
 @if($link)
+<div class="hidden-xs">
 	<h3><a href="{{ url('advising/index/' . $advisor->id)}}">{{ $advisor->name }}</a> <small>{{ $advisor->department->name }}</small></h3>
+</div>
+<div class="visible-xs-block">
+	<h3><a href="{{ url('advising/index/' . $advisor->id)}}">{{ $advisor->name }}</a><br><small>{{ $advisor->department->name }}</small></h3>
+</div>
 @else
+<div class="hidden-xs">
 	<h3>{{ $advisor->name }} <small>{{ $advisor->department->name }}</small></h3>
-	<a href="{{ url('advising/select') }}" class="btn btn-primary pull-right">Select a Different Advisor</a>
+</div>
+<div class="visible-xs-block">
+	<h3>{{ $advisor->name }}<br><small>{{ $advisor->department->name }}</small></h3>
+</div>
 @endif
-	<p><a href="mailto:{{ $advisor->email }}">{{ $advisor->email }}</a></p>
-	<p><b>Office: </b>{{ $advisor->office }}<br>
+	<p><b>Email: </b><a href="mailto:{{ $advisor->email }}">{{ $advisor->email }}</a><br>
+	<b>Office: </b>{{ $advisor->office }}<br>
 	<b>Phone: </b>{{ $advisor->phone }}<br>
 	<b>Notes: </b>{{ $advisor->notes }}</p>
-</div>
+@if(!$link)
+	<p><a href="{{ url('advising/select') }}" class="btn btn-primary">Select a Different Advisor</a></p>
+@endif
 </div>
